c174c0d9d9e13e59908c92be3fb1197ba21a1448
Ashraf <ashraf@Ashrafs-MacBook-Pro.local>
2023-10-17 12:11:44 +0100

    + wpm and restart options

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,24 @@ import Confetti from "react-confetti";
 // import useWindowSize from "react-use/lib/useWindowSize";
 const regex = /^[a-z ]$/;
 function App() {
-  const [text, setText] = useState(generateText());
+  const [text, setText] = useState("");
   const [typed, setTyped] = useState("");
   const [gameStarted, setGameStarted] = useState(false);
   const [startTime, setStartTime] = useState(0);
+  const [userDone, setUserDone] = useState(false);
   const [wpm, setWpm] = useState(0);
   // const { width, height } = useWindowSize();
-  console.log("typed", typed);
   useEffect(() => {
     if (typed.length === text.length) {
+      setUserDone(true);
       const endTime = new Date().getTime();
       const correctWords = typed
         .split(" ")
         .reduce((acc, letter, i) => (letter === text.split(" ")[i] ? acc + 1 : acc), 0);
-      console.log("correctWords", correctWords);
       const wordsPerMin = Math.round(correctWords / ((endTime - startTime) / 1000 / 60));
       setWpm(wordsPerMin);
     }
   }, [typed, text]);
-
-  console.log(
-    "correct words",
-    typed
-      .split(" ")
-      .reduce((acc, letter, i) => (letter === text.split(" ")[i] ? acc + 1 : acc), 0)
-  );
 
   const handleKeydown: KeyboardEventHandler = (e) => {
     if (typed === "") setStartTime(new Date().getTime());
@@ -36,13 +29,23 @@ function App() {
     if (enteredKey === "backspace") setTyped(typed.substring(0, typed.length - 1));
     if (regex.test(enteredKey)) setTyped(typed + enteredKey);
   };
+  const handleStart = () => {
+    setText(generateText());
+    setGameStarted(true);
+  };
+  const handleEnd = () => {
+    setGameStarted(false);
+    setTyped("");
+    setUserDone(false);
+    setWpm(0);
+  };
   return (
     <div className=" w-screen h-screen flex flex-col gap-4 items-center justify-center">
       {!gameStarted && (
         <>
           <h1 className=" text-5xl">Typing Game</h1>
           <button
-            onClick={() => setGameStarted(true)}
+            onClick={handleStart}
             className=" text-2xl px-8 py-1 bg-sky-900 rounded-md shadow-lg mt-4"
           >
             Start
@@ -51,13 +54,13 @@ function App() {
       )}
       {gameStarted && (
         <>
-          {wpm > 0 && (
+          {userDone && (
             <h3 className=" text-4xl font-light border-zinc-400 rounded-lg shadow-md shadow-slate-600 mb-4 px-6 py-2">
               {" "}
               <span className=" font-extrabold"> {wpm}</span> word per minute
             </h3>
           )}
-          {wpm > 2 && <Confetti />}
+          {wpm > 0 && <Confetti />}
           <p className=" text-4xl px-8">
             {text.split("").map((letter, i) => (
               <span
@@ -76,10 +79,7 @@ function App() {
             ))}
           </p>
           <button
-            onClick={() => {
-              setGameStarted(false);
-              setTyped("");
-            }}
+            onClick={handleEnd}
             className=" m-8 z-40 text-xl px-8 py-1 bg-sky-900 rounded-md shadow-md shadow-slate-700"
           >
             End
